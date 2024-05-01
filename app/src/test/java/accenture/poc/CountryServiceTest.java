7f8a1e0d6754e8b2c263e46be4ceb9b482183fc5
lena250401 <lena250401@gmail.com>
2024-05-01 09:56:40 +0200

    ✨ Generating a jar file of the project

--- app/src/test/java/accenture/poc/CountryServiceTest.java
+++ app/src/test/java/accenture/poc/CountryServiceTest.java
@@ -1,12 +1,7 @@
 package accenture.poc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
